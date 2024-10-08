831668272a67374713749e9f29b20841a5db8dff
Mali Revanth Reddy <118295254+RevanthMali@users.noreply.github.com>
2024-10-08 23:43:08 +0530

    Create fork.c

---
+++ fork.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<wait.h>
+#include<unistd.h>
+void main()
+{
+  pid_t pid ;
+  pid = fork();
+  if(pid==0){
+    printf("child process id:%d",getpid());
+    printf("parent process id:%d",getppid());
+  }else if(pid>0){
+    printf("parent process id:%d",getpid());
+    printf("child process id:%d",pid());
+  }else{
+    printf("fork failed!");
+  }
+
+}
